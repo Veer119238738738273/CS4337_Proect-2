2da4574e6b5567b1b436d866c6a24169b1d96f2f
Veer119238738738273 <47932972+Veer119238738738273@users.noreply.github.com>
2025-12-07 22:11:52 -0600

    updated valid_map

--- project.pl
+++ project.pl
@@ -4,10 +4,13 @@ find_exit(_, _) :-
     write('find_exit not implemented yet'), nl, fail.
 
 valid_map(Map, Start, Exit) :-
-    member(Row, Map),
-    member(s, Row), Start = (0,0),
-    member(Row2, Map),
-    member(e, Row2), Exit = (1,1).
+    Map \= [],
+    rectangular(Map),
+    all_valid_cells(Map),
+    findall((R,C), cell_at(Map, R, C, s), Starts),
+    findall((R,C), cell_at(Map, R, C, e), Exits),
+    Starts = [Start],
+    Exits = [Exit].
 
 cell_at(Map, R, C, Val) :-
     nth0(R, Map, Row),
