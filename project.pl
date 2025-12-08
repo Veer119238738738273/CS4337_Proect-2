6d7c82949eb2b30d8c3afd489c9aea1f147b2c73
Veer119238738738273 <47932972+Veer119238738738273@users.noreply.github.com>
2025-12-07 20:38:52 -0600

    Working on Map Start

--- project.pl
+++ project.pl
@@ -3,3 +3,9 @@
 find_exit(_, _) :-
     write('find_exit not implemented yet'), nl, fail.
 
+valid_map(Map, Start, Exit) :-
+    member(Row, Map),
+    member(s, Row), Start = (0,0),
+    member(Row2, Map),
+    member(e, Row2), Exit = (1,1).
+
